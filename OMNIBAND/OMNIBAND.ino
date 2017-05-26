1b248462a78bfeda8fa2cba7588879aa1a944e18
Gian Lazzarini <gianlazzarini@gmail.com>
2017-05-25 18:07:44 -0700

    Moved the /****/ comment section up by the top with the other one.

--- OMNIBAND/OMNIBAND.ino
+++ OMNIBAND/OMNIBAND.ino
@@ -11,6 +11,23 @@ This an Arduino project. It's Best Way to Get Started with your PulseSensor™ &
 6) "Human-readable" code that is newbie friendly." 
 */
 
+/*************************************************** 
+  This is a library example for the MLX90614 Temp Sensor
+
+  Designed specifically to work with the MLX90614 sensors in the
+  adafruit shop
+  ----> https://www.adafruit.com/products/1748
+  ----> https://www.adafruit.com/products/1749
+
+  These sensors use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
 
 //Testing 1234 Hello this is Gian.
 // Testing as Gian from Gian's computer
@@ -52,23 +69,10 @@ delay(10);
    
 }
 
-/*************************************************** 
-  This is a library example for the MLX90614 Temp Sensor
 
-  Designed specifically to work with the MLX90614 sensors in the
-  adafruit shop
-  ----> https://www.adafruit.com/products/1748
-  ----> https://www.adafruit.com/products/1749
 
-  These sensors use I2C to communicate, 2 pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+
 
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
