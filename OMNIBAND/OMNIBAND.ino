8f091641eccdf4bba3d0bafcf50447b2c9e4031c
Gian Lazzarini <gianlazzarini@gmail.com>
2017-05-27 16:19:22 -0700

    Merged accelerometer main loop code in

--- OMNIBAND/OMNIBAND.ino
+++ OMNIBAND/OMNIBAND.ino
@@ -113,41 +113,8 @@ delay(10);
   Serial.println();
   delay(500);
   //
-
+  
   // For Accelerometer
-  #ifndef ESP8266
-  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
-#endif
-  Serial.begin(9600);
-
-  // Try to initialise and warn if we couldn't detect the chip
-  if (!lsm.begin())
-  {
-    Serial.println("Oops ... unable to initialize the LSM303. Check your wiring!");
-    while (1);
-  }
-  //
-   
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void loop()
-{
   lsm.read();
   Serial.print("AX: "); Serial.print((int)lsm.accelData.x); Serial.print(" ");
   Serial.print("AY: "); Serial.print((int)lsm.accelData.y); Serial.print(" ");
@@ -156,4 +123,5 @@ void loop()
   Serial.print("MY: "); Serial.print((int)lsm.magData.y);   Serial.print(" ");
   Serial.print("MZ: "); Serial.println((int)lsm.magData.z); Serial.print(" ");
   delay(100);
+  //
 }
