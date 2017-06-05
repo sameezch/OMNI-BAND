5ce78d206610e732b68cb8719c7027f64ed69d87
Gian Lazzarini <gianlazzarini@gmail.com>
2017-06-04 20:43:50 -0700

    Removed unceassary temp data read out
    Made it easier for parsing

--- OMNIBAND/OMNIBAND.ino
+++ OMNIBAND/OMNIBAND.ino
@@ -100,7 +100,7 @@ delay(10);
   //Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
   //Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
   //Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
-  Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
+  Serial.print("Object = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
 
   Serial.println();
   delay(500);
