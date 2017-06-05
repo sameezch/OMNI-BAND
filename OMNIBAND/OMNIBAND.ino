7abdd851014b4d3857b4a43df2c5e72eb3e7c0c9
Gian Lazzarini <gianlazzarini@gmail.com>
2017-06-04 20:40:48 -0700

    Removed ambient and Celecius read out
    For ease of parsing

--- OMNIBAND/OMNIBAND.ino
+++ OMNIBAND/OMNIBAND.ino
@@ -97,9 +97,9 @@ void loop() {
 delay(10);
 
    // For Temp Sensor
-  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
-  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
-  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
+  //Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
+  //Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+  //Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
   Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
 
   Serial.println();
